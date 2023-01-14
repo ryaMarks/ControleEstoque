1d6d108235ef01982438e2c2396ba521d75bbcda
Ryam Marques <ryab08340gmail.com>
2023-01-14 16:30:49 -0300

    cria formulario para cadastrar produto

--- projeto/produto/views.py
+++ projeto/produto/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
 from .models import Produto
+from .forms import ProdutoForm
 # Create your views here.
 
 
@@ -21,3 +23,10 @@ def produto_detail(request, pk):
         obj = obj.filter(produto__icontains=search)
     context = {'object': obj}
     return render(request, template_name, context)
+
+
+
+class ProdutoCreate(CreateView):
+    model = Produto
+    template_name = 'produto_form.html'
+    form_class = ProdutoForm
--- projeto/settings.py
+++ projeto/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # apps de terceiros
+    'widget_tweaks',
     # meus apps
     'projeto.core',
     'projeto.produto',
