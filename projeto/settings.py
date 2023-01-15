e3a544ebf1c2de83ae55ce49766353eb041ea915
Ryam Marques <ryab08340gmail.com>
2023-01-15 15:50:58 -0300

    adicionando formulario de entrada de produtos no estoque

--- projeto/settings.py
+++ projeto/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # apps de terceiros
+    'bootstrapform',
     'widget_tweaks',
     # meus apps
     'projeto.core',
