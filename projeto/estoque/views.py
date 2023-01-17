eb1adf383c3e57ddf6d0274a977745e1ecd0e503
Ryam Marques <ryab08340gmail.com>
2023-01-17 05:22:10 -0300

    criada a lista de saida do estoque

--- projeto/estoque/views.py
+++ projeto/estoque/views.py
@@ -3,19 +3,19 @@ from django.forms import inlineformset_factory
 from django.http import HttpResponseRedirect
 from ..produto.models import Produto
 from .forms import EstoqueForm, EstoqueItensForm
-from .models import Estoque, EstoqueItens
+from .models import EstoqueEntrada, EstoqueSaida, EstoqueItens
 
 # Create your views here.
 def estoque_entrada_list(request):
     template_name = 'estoque_entrada_list.html'
-    objects = Estoque.objects.filter(movimento='e')
+    objects = EstoqueEntrada.objects.all()
     context = {'object_list': objects}
     return render(request, template_name, context=context)
 
 
 def estoque_entrada_detail(request, pk):
     template_name = 'estoque_entrada_detail.html'
-    obj = Estoque.objects.get(pk=pk)
+    obj = EstoqueEntrada.objects.get(pk=pk)
     context = {'object': obj}
     return render(request, template_name, context)
 
@@ -31,9 +31,9 @@ def dar_baixa_estoque(form):
 
 def estoque_entrada_add(request):
     template_name = 'estoque_entrada_form.html'
-    estoque_form = Estoque()
+    estoque_form = EstoqueEntrada()
     item_estoque_formset = inlineformset_factory(
-        Estoque,
+        EstoqueEntrada,
         EstoqueItens,
         form=EstoqueItensForm,
         extra=0,
@@ -54,3 +54,10 @@ def estoque_entrada_add(request):
         formset = item_estoque_formset(instance=estoque_form, prefix='estoque')
     context = {'form': form, 'formset': formset}
     return render(request, template_name, context)
+
+
+def estoque_saida_list(request):
+    template_name = 'estoque_saida_list.html'
+    objects = EstoqueSaida.objects.all()
+    context = {'object_list': objects}
+    return render(request, template_name, context=context)
