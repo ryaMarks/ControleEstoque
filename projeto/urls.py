7642d651a75d0403cd7a1b0523847b86bc03afdd
Ryam Marques <ryab08340gmail.com>
2023-01-24 18:22:26 -0300

    upload

--- projeto/urls.py
+++ projeto/urls.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.views import logout_then_login
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,4 +11,4 @@ urlpatterns = [
     path('produto/', include('projeto.produto.urls')),
     path('estoque/', include('projeto.estoque.urls')),
     path('logout/', logout_then_login, name='logout'),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
